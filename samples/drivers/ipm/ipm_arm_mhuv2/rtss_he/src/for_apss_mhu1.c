8cf18082e4f6d8125958ad6bdfb563d9435c7581
Kari Haapalehto <kari.haapalehto@alifsemi.com>
2026-04-17 10:23:20 +0300

    IPC using MHU
    zephyr.patch is changes needed for zephyr
    linux_mpu_test_hp.json is config for SE tool
    
    Signed-off-by: Kari Haapalehto <kari.haapalehto@alifsemi.com>

--- samples/drivers/ipm/ipm_arm_mhuv2/rtss_he/src/for_apss_mhu1.c
+++ samples/drivers/ipm/ipm_arm_mhuv2/rtss_he/src/for_apss_mhu1.c
@@ -1,93 +1,122 @@
-/* Copyright (C) 2024 Alif Semiconductor - All Rights Reserved.
- * Use, distribution and modification of this code is permitted under the
- * terms stated in the Alif Semiconductor Software License Agreement
- *
- * You should have received a copy of the Alif Semiconductor Software
- * License Agreement with this file. If not, please write to:
- * contact@alifsemi.com, or visit: https://alifsemi.com/license
- *
- */
+/* Copyright (C) 2024 Alif Semiconductor */
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/ipm.h>
 
 #define ITERATIONS 10
-const struct device *mhu1_r;
-const struct device *mhu1_s;
+
+/*
+ * M55 HE <-> A32 Communication Test (MHU1)
+ *
+ * Aliases must be defined in DTS:
+ * apsshemhu1r -> MHU1 Receiver (from A32)
+ * apsshemhu1s -> MHU1 Sender (to A32)
+ */
+
+const struct device *mhu1_r;  // MHU1 Receiver from A32
+const struct device *mhu1_s;  // MHU1 Sender to A32
 
 static volatile bool msg_sent;
 static volatile bool msg_received;
 static uint32_t tx_msg;
 
-static void recv_cb(const struct device *mhuv2_ipmdev, void *user_data,
-		uint32_t id, volatile void *data)
+/* Callback when data received from A32 */
+static void recv_cb(const struct device *dev, void *user_data,
+                    uint32_t id, volatile void *data)
 {
-	ARG_UNUSED(mhuv2_ipmdev);
-	ARG_UNUSED(user_data);
+    ARG_UNUSED(dev);
+    ARG_UNUSED(user_data);
 
-	printk("RTSS-HE: MSG rcvd on ch:%d is 0x%x\n", id, *((uint32_t *)data));
-	msg_received = true;
+    /* Print received data */
+    printk("M55-HE: RX MHU1 Ch%d = 0x%x\n", id, *((uint32_t *)data));
+    msg_received = true;
 }
 
-static void send_cb(const struct device *mhuv2_ipmdev, void *user_data,
-		uint32_t id, volatile void *data)
+/* Callback when data sent to A32 */
+static void send_cb(const struct device *dev, void *user_data,
+                    uint32_t id, volatile void *data)
 {
-	ARG_UNUSED(mhuv2_ipmdev);
-	ARG_UNUSED(user_data);
-	ARG_UNUSED(data);
+    ARG_UNUSED(dev);
+    ARG_UNUSED(user_data);
+    ARG_UNUSED(data);
 
-	printk("RTSS-HE: MSG sent on Ch:%d is 0x%x\n", id, tx_msg);
-	msg_sent = true;
+    printk("M55-HE: TX MHU1 Ch%d Done\n", id);
+    msg_sent = true;
 }
 
-static void send(void)
+/* Send reply to A32 */
+static void send_reply(void)
 {
-	int wait = 0;
-	int size = 4;
+    /*
+     * Send replies on MHU1 Channel 0.
+     * A32 Linux should listen on corresponding MHU1 rx endpoint.
+     */
 
-	msg_received = false;
-	msg_sent     = false;
-	tx_msg       = 0xF00DF00D;
+    /* Send 1st Message */
+    msg_sent = false;
+    tx_msg = 0xaa55aa00;  // Different data pattern for MHU1
+    printk("M55-HE: Sending 0x%x on MHU1 Ch0...\n", tx_msg);
+    ipm_send(mhu1_s, 0, 0, &tx_msg, 4);
+    while (!msg_sent);
 
-	ipm_send(mhu1_s, wait, 0, &tx_msg, size);
-	while (!msg_sent)
-		;
-	msg_sent     = false;
-	tx_msg       = 0xCAFECAFE;
-	ipm_send(mhu1_s, wait, 1, &tx_msg, size);
-	while (!msg_sent)
-		;
-	msg_sent = false;
+    /* Send 2nd Message */
+    msg_sent = false;
+    tx_msg = 0xaa55aa01;  // Different data pattern for MHU1
+    printk("M55-HE: Sending 0x%x on MHU1 Ch0...\n", tx_msg);
+    ipm_send(mhu1_s, 0, 0, &tx_msg, 4);
+    while (!msg_sent);
 }
 
 int main(void)
 {
-	uint32_t recv_data;
-	int i = 0;
+    uint32_t recv_data;
+    int i = 0;
 
-	msg_received = false;
-	msg_sent = false;
-	printk("RTSS-HE A32 MHU 1 example on %s\n", CONFIG_BOARD);
-	mhu1_r = DEVICE_DT_GET(DT_NODELABEL(apss_rtsshe_mhu1_r));
-	mhu1_s = DEVICE_DT_GET(DT_NODELABEL(rtsshe_apss_mhu1_s));
-	if (!device_is_ready(mhu1_r) || !device_is_ready(mhu1_s)) {
-		printk("MHU devices not ready\n");
-		return -1;
-	}
-	ipm_register_callback(mhu1_r, recv_cb, &recv_data);
-	ipm_register_callback(mhu1_s, send_cb, NULL);
+    printk("\n========================================\n");
+    printk("M55-HE <-> A32 (APSS) MHU1 Test\n");
+    printk("========================================\n");
 
-	/* Enable Rx MHU interrupt */
-	ipm_set_enabled(mhu1_r, true);
+    /* Get device handles from DTS aliases */
+    mhu1_r = DEVICE_DT_GET(DT_ALIAS(apsshemhu1r));
+    mhu1_s = DEVICE_DT_GET(DT_ALIAS(apsshemhu1s));
 
-	while (i < ITERATIONS) {
-		while (!msg_received)
-			;
-		send();
-		++i;
-	}
-	/* Disable Rx MHU interrupt */
-	ipm_set_enabled(mhu1_r, false);
+    if (!device_is_ready(mhu1_r) || !device_is_ready(mhu1_s)) {
+        printk("ERROR: MHU1 devices not ready!\n");
+        return -1;
+    }
 
-	return 0;
+    printk("MHU1 devices ready\n");
+
+    /* Register callbacks */
+    ipm_register_callback(mhu1_r, recv_cb, &recv_data);
+    ipm_register_callback(mhu1_s, send_cb, NULL);
+
+    /* Enable receiver interrupt */
+    ipm_set_enabled(mhu1_r, true);
+
+    printk("Waiting for A32 messages on MHU1...\n\n");
+
+    /* Main loop: wait for A32, then reply */
+    while (i < ITERATIONS) {
+        msg_received = false;
+
+        /* Wait for A32 to send data */
+        while (!msg_received);
+
+        /* Give a small delay to ensure A32 is ready to receive */
+        k_sleep(K_MSEC(10));
+
+        /* Send reply */
+        send_reply();
+
+        printk("Iteration %d complete\n\n", i + 1);
+        ++i;
+    }
+
+    /* Disable receiver */
+    ipm_set_enabled(mhu1_r, false);
+
+    printk("MHU1 Test completed successfully!\n");
+    return 0;
 }
+
